9760c944586cbd5c730c2f5cd12888b8ffb25c6b
louis laporte <louis.laporte12@gmail.com>
2017-09-17 12:38:24 -0400

    Update to pep8

--- stock.py
+++ stock.py
@@ -11,8 +11,8 @@ from bokeh.io import curdoc
 from bokeh.models import HoverTool
 
 from bokeh.models.widgets import (Select, Slider, TextInput, RangeSlider, Div,
-                                CheckboxGroup, DataTable, DateFormatter, TableColumn,
-                                Panel, Tabs)
+                                  CheckboxGroup, DataTable, DateFormatter, TableColumn,
+                                  Panel, Tabs)
 
 from bokeh.models.widgets.inputs import DateRangeSlider
 from bokeh.client import push_session
@@ -22,54 +22,58 @@ with open('secret/quandl_key', 'r') as f:
     api_key = f.readline().replace('\n', '')
 quandl.ApiConfig.api_key = api_key
 
+
 def datetime(x):
     return np.array(x, dtype=np.datetime64)
 
+
 def get_data(ticker, start, end):
     return pd.DataFrame(
-                quandl.get_table(
-                    'WIKI/PRICES', ticker=ticker,
-                    date={'gte': start, 'lte': end}
-                )
+        quandl.get_table(
+            'WIKI/PRICES', ticker=ticker,
+            date={'gte': start, 'lte': end}
+        )
     )
 
+
 class Market():
-################################################################################
-# PLOT WIDGET
-################################################################################
-# +----------------------------+--------------+
-# |                            |              |
-# |          plot              |  checkbox    |
-# |                            |              |
-# +----------------------------+--------------+
-# | +-----------+ +----------+ |
-# | | start_btn | | end_btn  | |
-# | +-----------+ +----------+ |
-# +----------------------------+
-################################################################################
+    ################################################################################
+    # PLOT WIDGET
+    ################################################################################
+    # +----------------------------+--------------+
+    # |                            |              |
+    # |          plot              |  checkbox    |
+    # |                            |              |
+    # +----------------------------+--------------+
+    # | +-----------+ +----------+ |
+    # | | start_btn | | end_btn  | |
+    # | +-----------+ +----------+ |
+    # +----------------------------+
+    ################################################################################
+
     def __init__(self, start='2014-01-01', end='2014-06-01', tickers=['AAPL']):
         super().__init__()
         self.start = start
         self.end = end
         self.select_tick_btn = Select(
-                                    title="Company tick",
-                                    value=tickers[0],
-                                    options=tickers
+            title="Company tick",
+            value=tickers[0],
+            options=tickers
         )
         self.debug = Div(text="""Some text""", width=200, height=100)
 
-        ## /!\ not working: wait for bokeh master release 12.7
-        #date_range_slider = DateRangeSlider(bounds=(start, end),
+        # /!\ not working: wait for bokeh master release 12.7
+        # date_range_slider = DateRangeSlider(bounds=(start, end),
         #                                    value=(start, end),
         #                                    range=({'days': 1}, {'days':5})
-        #)
+        # )
         self.start_date_input = TextInput(value=start, title='start')
         self.end_date_input = TextInput(value=end, title='end')
         # layout
 
         self.display_plot_checkbox_group = CheckboxGroup(
-                labels=["open/close", "high/low", "volume", 'all'],
-                active=[3]
+            labels=["open/close", "high/low", "volume", 'all'],
+            active=[3]
         )
         self.plot_layout = self.candle_plot(tickers[0])
 
@@ -81,10 +85,8 @@ class Market():
             [self.plot_layout],
         ])
 
-
     def widget(self):
         return self.main_layout
-
 
     def normalize_name(self):
         self.df.columns = [c.lower().replace(' ', '_') for c in self.df.columns.values]
@@ -95,9 +97,9 @@ class Market():
 
         self.normalize_name()
         self.df = self.df.set_index(index_name)
-        print(self.df[['open', 'close', 'adj_open', 'adj_close', 'volume','split_ratio']])
+        print(self.df[['open', 'close', 'adj_open', 'adj_close', 'volume', 'split_ratio']])
         index = self.df.index.get_level_values(index_name)
-        data = {index_name: index }
+        data = {index_name: index}
 
         for val in self.df.columns.values:
             data[val] = self.df[val]
@@ -106,44 +108,44 @@ class Market():
 #
         hover = HoverTool(
             tooltips=[
-                ( 'date',      '@date{%F}'         ),
-                ( 'adj close', '$@adj_close{%0.2f}'), # use @{ } for field names with spaces
-                ( 'adj open', '$@adj_open{%0.2f}'), # use @{ } for field names with spaces
-                ( 'volume',    '@volume{0.00 a}'   ),
-                ( 'open',      '@open{%0.2f}'      ),
-                ( 'close',     '@close{%0.2f}'     ),
+                ('date',      '@date{%F}'),
+                ('adj close', '$@adj_close{%0.2f}'),  # use @{ } for field names with spaces
+                ('adj open',  '$@adj_open{%0.2f}'),  # use @{ } for field names with spaces
+                ('volume',    '@volume{0.00 a}'),
+                ('open',      '@open{%0.2f}'),
+                ('close',     '@close{%0.2f}'),
             ],
             formatters={
-                'date'      : 'datetime', # use 'datetime' formatter for 'date' field
-                'adj_close' : 'printf',   # use 'printf' formatter for 'adj close' field
-                'adj_open' : 'printf',   # use 'printf' formatter for 'adj close' field
-                'open' : 'printf',   # use 'printf' formatter for 'adj close' field
-                'close' : 'printf',   # use 'printf' formatter for 'adj close' field
+                'date': 'datetime',  # use 'datetime' formatter for 'date' field
+                'adj_close': 'printf',  # use 'printf' formatter for 'adj close' field
+                'adj_open': 'printf',  # use 'printf' formatter for 'adj close' field
+                'open': 'printf',  # use 'printf' formatter for 'adj close' field
+                'close': 'printf',  # use 'printf' formatter for 'adj close' field
             },
             # display a tooltip whenever the cursor is vertically in line with a glyph
             mode='vline'
         )
         inc = self.df['close'] > self.df['open']
         dec = self.df['open'] > self.df['close']
-        w = 12*60*60*1000 # half day in ms
+        w = 12 * 60 * 60 * 1000  # half day in ms
         p = figure(x_axis_type="datetime", plot_width=1600, title=ticker, tools=[hover])
-        p.grid.grid_line_alpha=0.3
-        p.xaxis.major_label_orientation = pi/4
+        p.grid.grid_line_alpha = 0.3
+        p.xaxis.major_label_orientation = pi / 4
         p.xaxis.axis_label = 'Date'
         p.yaxis.axis_label = 'Price'
         p.grid.grid_line_alpha = 0.3
 
         p.line(index_name, 'adj_close', color='#A6CEE3', source=source)
-        p.line(index_name, 'adj_open', color='#FB9A99',source=source)
+        p.line(index_name, 'adj_open', color='#FB9A99', source=source)
         p.segment(index_name, 'high', index_name, 'low', color="white", source=source)
 
         p.vbar(
-                index[inc], w, self.df['open'][inc], self.df['close'][inc],
-                fill_color="#D5E1DD", line_color="white"
+            index[inc], w, self.df['open'][inc], self.df['close'][inc],
+            fill_color="#D5E1DD", line_color="white"
         )
         p.vbar(
-                index[dec], w, self.df['open'][dec], self.df['close'][dec],
-                fill_color="#F2583E", line_color="white"
+            index[dec], w, self.df['open'][dec], self.df['close'][dec],
+            fill_color="#F2583E", line_color="white"
         )
         p.legend.location = "top_left"
         p.background_fill_color = "black"
@@ -155,30 +157,30 @@ class Market():
                 columns.append(TableColumn(field=key, title=key))
 
         data_table = DataTable(source=source, columns=columns, width=1600)
+        layout = column(p, data_table)
+        return layout
 
-        l = column(p, data_table)
-        return l
 
-    #def month_average(self):
-    #    aapl = np.array(AAPL['adj_close'])
-    #    aapl_dates = np.array(AAPL['date'], dtype=np.datetime64)
+    # def month_average(self):
+    #     aapl = np.array(AAPL['adj_close'])
+    #     aapl_dates = np.array(AAPL['date'], dtype=np.datetime64)
 
-    #    window_size = 30
-    #    window = np.ones(window_size)/float(window_size)
-    #    aapl_avg = np.convolve(aapl, window, 'same')
+    #     window_size = 30
+    #     window = np.ones(window_size)/float(window_size)
+    #     aapl_avg = np.convolve(aapl, window, 'same')
 
-    #    p = figure(x_axis_type="datetime", title="AAPL One-Month Average")
-    #    p.grid.grid_line_alpha = 0
-    #    p.xaxis.axis_label = 'Date'
-    #    p.yaxis.axis_label = 'Price'
-    #    p.ygrid.band_fill_color = "olive"
-    #    p.ygrid.band_fill_alpha = 0.1
+    #     p = figure(x_axis_type="datetime", title="AAPL One-Month Average")
+    #     p.grid.grid_line_alpha = 0
+    #     p.xaxis.axis_label = 'Date'
+    #     p.yaxis.axis_label = 'Price'
+    #     p.ygrid.band_fill_color = "olive"
+    #     p.ygrid.band_fill_alpha = 0.1
 
-    #    p.circle(aapl_dates, aapl, size=4, legend='close',
-    #             color='darkgrey', alpha=0.2)
+    #     p.circle(aapl_dates, aapl, size=4, legend='close',
+    #              color='darkgrey', alpha=0.2)
 
-    #    p.line(aapl_dates, aapl_avg, legend='avg', color='navy')
-    #    p.legend.location = "top_left"
+    #     p.line(aapl_dates, aapl_avg, legend='avg', color='navy')
+    #     p.legend.location = "top_left"
     ############################################################################
     # CALLBACK
     ############################################################################
@@ -194,14 +196,16 @@ class Market():
         self.end = new
         self.main_layout.children[1] = self.candle_plot(self.select_tick_btn.value)
 
+
 def MainWindow():
     companies = ['AAPL', 'IBM', 'GOOGL', 'MSFT']
     market = Market(tickers=companies)
     tab1 = Panel(child=market.widget(), title="Market")
-    #tab2 = Panel(title="live", child=layout([l]))
-    tabs = Tabs(tabs=[ tab1])#, tab2 ])
+    # tab2 = Panel(title="live", child=layout([l]))
+    tabs = Tabs(tabs=[tab1])  # , tab2 ])
 # stream some data
     return tabs
+
 
 p = figure(plot_width=400, plot_height=400)
 r1 = p.line([], [], color="firebrick", line_width=2)
@@ -209,20 +213,39 @@ r2 = p.line([], [], color="navy", line_width=2)
 
 ds1 = r1.data_source
 ds2 = r2.data_source
+p.xaxis.visible = False
+
+
+p2 = figure(plot_width=400, plot_height=400)
+r3 = p2.line([], [], color="firebrick", line_width=2)
+r4 = p2.line([], [], color="navy", line_width=2)
+
+ds3 = r3.data_source
+ds4 = r4.data_source
+
 
 @linear()
 def update(step):
     ds1.data['x'].append(step)
-    ds1.data['y'].append(random.randint(0,100))
+    ds1.data['y'].append(random.randint(0, 100))
     ds2.data['x'].append(step)
-    ds2.data['y'].append(random.randint(0,100))  
+    ds2.data['y'].append(random.randint(0, 100))
     ds1.trigger('data', ds1.data, ds1.data)
     ds2.trigger('data', ds2.data, ds2.data)
 
-curdoc().add_root(p)
+    ds3.data['x'].append(step)
+    ds3.data['y'].append(random.randint(0, 100))
+    ds4.data['x'].append(step)
+    ds4.data['y'].append(random.randint(0, 100))
+    ds3.trigger('data', ds3.data, ds3.data)
+    ds4.trigger('data', ds4.data, ds4.data)
+
+
+grid_layout = gridplot(children=[[p], [p2]], sizing_mode='stretch_both', merge_tools=True)
+curdoc().add_root(grid_layout)
 
 # Add a periodic callback to be run every 500 milliseconds
 curdoc().add_periodic_callback(update, 500)
 
 
-#curdoc().add_root(MainWindow())
+# curdoc().add_root(MainWindow())
