52d88249a7a28ee62f6d9f201bca39eacba45fff
louis laporte <louis.laporte12@gmail.com>
2017-11-04 18:41:16 -0400

    Add methods for table

--- core/market.py
+++ core/market.py
@@ -8,10 +8,19 @@ import json
 import logging
 from scrapy.selector import Selector
 
+log = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(name)s][%(levelname)s]%(message)s')
+ch.setFormatter(formatter)
+log.addHandler(ch)
+
+store_file = os.path.realpath(os.path.dirname(__file__) + '/../dataset/sp500.h5')
+
 
 class SP500:
 
-    def __init__(self, store_file):
+    def __init__(self, store_file=store_file):
         """
         Construtor for SP500
 
@@ -19,25 +28,63 @@ class SP500:
         """
         self._load_quandl_api_key()
         if not os.path.exists(store_file):
-            self.tickers_store = pd.HDFStore(store_file)
+            self.store = pd.HDFStore(store_file)
         else:
-            self.tickers_store = pd.HDFStore(store_file, mode='r+')
-        print(self.tickers_store.keys())
-        print(self.tickers_store)
+            self.store = pd.HDFStore(store_file, mode='r+')
+        log.info(self.store.keys())
+        log.info(self.store)
 
-        if '/default' not in self.tickers_store.keys():
+        if '/default' not in self.store.keys():
             self.save_default_info()
-        if '/website' not in self.tickers_store.keys():
+        if '/website' not in self.store.keys():
             self.save_websites()
-        if '/social' not in self.tickers_store.keys():
+        if '/social' not in self.store.keys():
             self.save_social_accounts()
 
     def __exit__(self):
-        self.tickers_store.close()
+        self.store.close()
+
+    def get_store(self):
+        """
+        Get store tables
+
+        :return: tables name
+        :rtype: list
+        """
+        return self.store.keys()
+
+    def get_table(self, table):
+        """
+        Get table from store
+
+        :param table: table name
+        :type table: str
+        :return: table info
+        :rtype: pandas.DataFrame
+        """
+        if not table.startswith("/"):
+            table = "/" + table
+        if table not in self.get_store():
+            raise ValueError("Table {} not found".format(table))
+        return self.store[table]
+
+    def get_table_header(self, table):
+        """
+        Get table from store
+
+        :param table: table name
+        :type table: str
+        :return: table header
+        :rtype: list
+        """
+        return self.get_table(table).columns.values
+
+    def update_store(self):
+        raise NotImplementedError
 
     @staticmethod
     def _load_quandl_api_key():
-        with open('secret/quandl_key.json', 'r') as f:
+        with open('../secret/quandl_key.json', 'r') as f:
             api_key = json.load(f)
         quandl.ApiConfig.api_key = api_key['key']
 
@@ -54,6 +101,7 @@ class SP500:
         header = ["symbol", "security_name", "gcis_sector", "gcis_subsector",
                   "hq_address", "date_added", "cik"]
         wikipedia_header = ["symbol", "link"]
+
         for row in table.findAll('tr')[1:]:
             cells = row.findAll('td')
             ticker = [h.text for h in cells if h.text != 'reports']
@@ -63,8 +111,8 @@ class SP500:
 
         wikipedia_df = pd.DataFrame(wikipedia, columns=wikipedia_header)
         tickers_df = pd.DataFrame(tickers, columns=header)
-        self.tickers_store['default'] = tickers_df
-        self.tickers_store['wikipedia'] = wikipedia_df
+        self.store['default'] = tickers_df
+        self.store['wikipedia'] = wikipedia_df
 
     def get_website(self, symbol):
         """
@@ -75,7 +123,7 @@ class SP500:
         :return: website for the given symbol
         :rtype: str
         """
-        tsw = self.tickers_store['website']
+        tsw = self.store['website']
         return tsw[tsw['symbol'] == symbol].loc[0]["link"]
 
     def update_websites(self):
@@ -85,7 +133,7 @@ class SP500:
         """
         Save websites url in hdf5 database
         """
-        wiki_df = self.tickers_store['wikipedia']
+        wiki_df = self.store['wikipedia']
         header = ["symbol", "link"]
         websites = []
         error_symbol = []
@@ -105,14 +153,14 @@ class SP500:
             else:
                 if website.startswith('//'):
                     website = 'http:' + website
-            print(row['symbol'], website)
+            log.debug(row['symbol'], website)
             websites.append([row['symbol'], website])
         website_df = pd.DataFrame(websites, columns=header)
-        self.tickers_store['website'] = website_df
-        print("Website error for symbols: {}".format(error_symbol))
+        self.store['website'] = website_df
+        log.debug("Website error for symbols: {}".format(error_symbol))
 
     def get_social_account(self, symbol, social_name):
-        tsw = self.tickers_store['social']
+        tsw = self.store['social']
         return tsw[tsw['symbol'] == symbol].loc[0]["twitter_account"]
 
     def update_social_accounts(self):
@@ -122,31 +170,35 @@ class SP500:
         """
         Save social accounts names in hdf5 database
         """
-        website_df = self.tickers_store['website']
+        website_df = self.store['website']
         header = ["symbol", "twitter_account"]
         accounts = []
         error_symbol = []
         for idx, row in website_df.iterrows():
-            print(row['link'])
+            log.debug(row['link'])
             try:
                 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1) \
-                AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"}
+                    AppleWebKit/537.36 (KHTML, like Gecko) \
+                    Chrome/41.0.2228.0 Safari/537.36"}
                 resp = requests.get(row['link'], verify=True, headers=headers, timeout=15)
                 resp.raise_for_status()
                 response = Selector(text=resp.text, type='html')
-                account = response.xpath('//a[re:test(@href, ".*twitter.com/(#!/)?(@)?\w+$")]//@href').extract_first()
+                account = response.xpath(
+                    '//a[re:test(@href, ".*twitter.com/(#!/)?(@)?\w+$")]//@href'
+                )
+                account = account.extract_first()
                 if account:
                     accounts.append(account)
             except Exception as err:
-                print(err)
+                log.debug(err)
                 error_symbol.append(row['symbol'])
                 continue
-        print(len(accounts))
+        log.debug(len(accounts))
         for account in accounts:
-            print(account)
+            log.debug(account)
         social_df = pd.DataFrame(accounts, columns=header)
-        self.tickers_store['social'] = social_df
-        print("Website error for symbols: {}".format(error_symbol))
+        self.store['social'] = social_df
+        log.debug("Website error for symbols: {}".format(error_symbol))
 
     def get_sectors(self):
         """
@@ -155,7 +207,7 @@ class SP500:
         :return: list of unique sectors
         :rtype: list
         """
-        return self.tickers_store['default']["gics_sector"].unique().tolist()
+        return self.store['default']["gics_sector"].unique().tolist()
 
     def get_tickers_by_sectors(self, sectors):
         """
@@ -170,7 +222,7 @@ class SP500:
             raise TypeError("Sectors must be a list")
         if len(set(sectors) & set(self.get_sectors())) != len(sectors):
             raise ValueError("Unknown sectors: {}".format(sectors))
-        tst = self.tickers_store['default']
+        tst = self.store['default']
         return tst[tst["gics_sector"].isin(sectors)]
 
     def get_tickers_symbol(self):
@@ -180,7 +232,7 @@ class SP500:
         :return: list of unique sectors
         :rtype: list
         """
-        return self.tickers_store["default"]["symbol"].tolist()
+        return self.store["default"]["symbol"].tolist()
 
     @staticmethod
     def get_ticker_stocks(ticker, start, end):
@@ -196,6 +248,8 @@ class SP500:
         :return: ticker stocks between start and end
         :rtype: pandas.DataFrame
         """
-        return pd.DataFrame(quandl.get_table('WIKI/PRICES',
-                                             ticker=ticker,
-                                             date={'gte': start, 'lte': end}))
+        return pd.DataFrame(
+            quandl.get_table('WIKI/PRICES',
+                             ticker=ticker,
+                             date={'gte': start, 'lte': end})
+        )
